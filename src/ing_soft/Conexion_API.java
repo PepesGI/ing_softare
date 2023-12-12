e21170d4ff4cd08551e0d6d965f14c8304a152d1
dexgon <153030285+dexgon@users.noreply.github.com>
2023-12-12 16:17:16 -0600

    Creacion de conexion API

---
+++ src/ing_soft/Conexion_API.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ing_soft;
+
+/**
+ *
+ * @author rarut
+ */
+public class Conexion_API {
+    public String api(String URL_credencial){
+    
+    
+        return null;
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
